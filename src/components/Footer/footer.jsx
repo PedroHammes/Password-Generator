ec3aa84dfbc0561f72c14ebd8da2ec272ac1121b
Pedro Hammes <hammespedro4@gmail.com>
2024-07-31 16:55:09 -0300

    final updates

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,9 +3,16 @@ import styles from './style.module.css'
 export default function Footer() {
     return (
         <footer>
-            <a href="#">Home</a>
+            <div className={styles.codedBy}>
+                <p>by <span>Pedro Hammes</span> </p>
+            </div>
+            <div className={styles.links}>
+                <a href="https://www.linkedin.com/in/pedro-hammes-7a6883208/" target='_blank'>LinkedIn</a>
+                <a href="https://github.com/PedroHammes" target='_blank'>GitHub</a>
+            </div>
+            {/* <a href="#">Home</a>
             <a href="#">New Password</a>
-            <a href="#">Profile</a>
+            <a href="#">Profile</a> */}
         </footer>
     )
 }
