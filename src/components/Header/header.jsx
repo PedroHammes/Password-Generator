7211098430f0a4f2afcd02ce51fe12f59941ffab
Pedro Hammes <hammespedro4@gmail.com>
2024-07-31 10:47:30 -0300

    header style

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 export default function Header(props) {
     return (
         <header className={styles.header}>
-            <h1>Good Morning, Aki!</h1>
+            <h1>Good Morning, <span className={styles.username}>Aki</span>!</h1>
             <img src={props.avatar} alt="" />
         </header>
     )
